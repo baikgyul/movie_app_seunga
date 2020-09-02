64cbe33874da9ff04483e91f02f8833948976a8e
unknown <gogohaha98@naver.com>
2020-09-02 12:28:25 +0900

    Updates

---
+++ precache-manifest.6f6113230ae95d97bb654e42bc5acb5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd77101539db761c4057256e91f70ff0",
+    "url": "/movie_app_seunga/index.html"
+  },
+  {
+    "revision": "bcc7eb1f864baf57b0e5",
+    "url": "/movie_app_seunga/static/css/main.d54fb6a3.chunk.css"
+  },
+  {
+    "revision": "51c7d8935f300d09578c",
+    "url": "/movie_app_seunga/static/js/2.85f90194.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/movie_app_seunga/static/js/2.85f90194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc7eb1f864baf57b0e5",
+    "url": "/movie_app_seunga/static/js/main.25353adb.chunk.js"
+  },
+  {
+    "revision": "7936b134bd22a48b9625",
+    "url": "/movie_app_seunga/static/js/runtime-main.1af193d8.js"
+  }
+]);
